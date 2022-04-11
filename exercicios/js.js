df995624a1164224ebd929400f5a8ebf4c1a2b49
TallesMadi <102757246+TallesMadi@users.noreply.github.com>
2022-04-11 13:40:21 -0300

    new ex

---
+++ exercicios/js.js
@@ -0,0 +1,15 @@
+var vel = 100
+if (vel > 80) {
+    console.log(`Você ultrapassou o limite de velocidade, pois está a ${vel} km/h!`)
+}
+else {
+    console.log(`Você está dentro do limite de velocidade, pois está a ${vel} km/h!`)
+}
+
+var n1 = 0
+if (n1 % 2 == 0 && n1 != 0) {
+    console.log(`O número ${n1} é par!`)
+}
+else {
+    console.log(`O número ${n1} é ímpar!`)
+}
